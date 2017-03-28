0ff7316096a397748fcea9e5536cb28232fd7f4a
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2017-03-28 09:55:46 -0600

    Use new API in test code

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ extern crate txtpic;
 use txtpic::calculate_string_brightness::calculate_string_brightness;
 
 fn main() {
-    println!("{}", calculate_string_brightness("@"));
-    println!("{}", calculate_string_brightness("%"));
-    println!("{}", calculate_string_brightness("#"));
-    println!("{}", calculate_string_brightness("x"));
-    println!("{}", calculate_string_brightness("+"));
-    println!("{}", calculate_string_brightness("="));
-    println!("{}", calculate_string_brightness(":"));
-    println!("{}", calculate_string_brightness("-"));
-    println!("{}", calculate_string_brightness("."));
-    println!("{}", calculate_string_brightness(" "));
+    println!("{}", calculate_string_brightness('M'));
+    println!("{}", calculate_string_brightness('@'));
+    println!("{}", calculate_string_brightness('%'));
+    println!("{}", calculate_string_brightness('#'));
+    println!("{}", calculate_string_brightness('x'));
+    println!("{}", calculate_string_brightness('+'));
+    println!("{}", calculate_string_brightness('='));
+    println!("{}", calculate_string_brightness(':'));
+    println!("{}", calculate_string_brightness('-'));
+    println!("{}", calculate_string_brightness('.'));
+    println!("{}", calculate_string_brightness(' '));
 }
