ce2a8044c1f46477b646976e915aa7b0be47a2e0
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2017-04-02 22:41:35 -0600

    Add additional documentation

--- src/text_image/image_to_text.rs
+++ src/text_image/image_to_text.rs
@@ -5,7 +5,51 @@ use character_set::CharacterSet;
 /// Converts an image to a TextImage using the given character set
 /// 
 /// `target_width` is a goal width - the actual width will be a value close to
-/// `target_width` that preserves the image's aspect ratio
+/// `target_width` that preserves the image's aspect ratio.
+/// 
+/// # Example
+/// ```
+/// # extern crate image;
+/// # extern crate txtpic_lib;
+/// # fn main() {
+/// # use self::image::open;
+/// # use std::path::Path;
+/// # use txtpic_lib::character_set::CharacterSet;
+/// # use txtpic_lib::text_image::image_to_text::image_to_text;
+/// // import necessary crates and modules...
+///
+/// let img = open(Path::new("./example/cat.jpg")).unwrap();
+/// let char_set = CharacterSet::preset_small();
+/// let txt_img = image_to_text(img, char_set, 80);
+/// let cute_kitty = "\
+/// ==xxxxxxx=======+++--++-+-.:--:--+:.-+---++++=xxxxxx=====xxxxxx==========++
+/// =xxxxxxx====xx=x=++=++++++.++-:--=+--++++++++=====xxx=++=xxxxxx============
+/// =xxxx=====xxxxxx+++++++=+--+x+:-=x+-+==+++=+===++=++++--+-+=xx====x========
+/// =====x+++xxxx=+++++=++++=+:-x+:-xx-:=x=+-+========+++++---+================
+/// +++++++--++++++=====++--=x=-=+-+=x--xxx+-+xxx======+++++---+===========++++
+/// --+-++---+++++======x=-:=xx-+=--+x-+xxx-:+x=+=======++++=+++=xx============
+/// +--+++---+++++x==+++=x+.-xx=-=--+=-+=xx=-=x=====xx=+++++==+===xxxxx========
+/// --++-++++++===xxx===+=x--xxx+++--+=++xxx+=+-+xxx-++--++++++++=xxxxxxxxxxxxx
+/// +++-+++=x=====x=++==++==+x=++++++++=+=xxxxx+-xxx++x=+=+++-+===xxxxxxxxxxxxx
+/// ++--+=++x====++=+xxx++xxx=xx+==++++==x%++xxxxxx+-x=++++=x=-++xxxxxxxxxxxxxx
+/// +++-++++++=xxxxx=+xxxxxx=:=x==+++++==xx+::-----+++--+===xxxx=xx%%%%%%%%%%%%
+/// +++--=+++xx===+=xx+++++---+=+=========+-++=++-----++==xxxxx+=xxxxxxxxxxxxxx
+/// ++---+===xxx==+++-++++===+-:-=xxx=xx==+:.--::---++==xx+--+++xxxxxxxxxxxxxxx
+/// +++++++=xxxxxx=+=++-------::+==xx====+++---++++++xxxxxx=x=xxxxxxx%%%%%%%%%%
+/// +----++++++++=xxxx=++++++-----+=====+--++-++=+==xxxxx=+=xxx%%%%%%%%%%%%xxxx
+/// +==+--=++++=====xxxxx===+++=--+===+=++--===+===xxxxx==xxxxxxxxx%%%%%%%%%%%%
+/// ++=+++++==++====xxxxxxx=+xxx=-==+++++=+=xx=x=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+/// -+++++++++========xxxxxx=x=xxxx=+x==-=xxxx===xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+/// +-+++--+++====xx=xxxxxxxxxxxxxxxx=-+xxxxxxxxxxxxxxxxxx========xxxxxxxxxxxxx
+/// ++--++++++++=x=x====xxxxxxxxxxxxxx+=xxxxxxxxxxxxxxxxx=++++++-+=xxxxxxxxxxxx
+/// ===+---+=+++++++=xxxxxxxxxxxxxxxxx==xxxxxxxxxxxxxxxxx==+++--+=====+xxxxxxxx
+/// +++++-----+++++==++==xx=++++=xxxxxxxxx%xxxxxxxxxxxxx=+++++--+=x=x===xxxxxxx
+/// ++++++------++=+-+=+=++++-+-+-++=xxxxxxxxxxxxxxxxxx+++----+++==xxx==xxxxxxx
+/// ++==++++--+--+--++++++++++++++++========+==+=xxxxxx=++-::--+==xxxxxxxxxxxxx
+/// ";
+/// assert_eq!(txt_img.to_string(), cute_kitty);
+/// # }
+/// ```
 pub fn image_to_text(img: DynamicImage, char_set: CharacterSet, target_width: u32) -> TextImage {
     let (img_width, img_height) = img.dimensions();
     let width_interval = closest_interval(img_width / target_width, img_width);
--- src/text_image/text_image.rs
+++ src/text_image/text_image.rs
@@ -1,6 +1,8 @@
+extern crate image;
+
 use std::fmt;
 use std::ops::{Index,IndexMut};
-use super::image::DynamicImage;
+use self::image::DynamicImage;
 use ::character_set::CharacterSet;
 use super::image_to_text::image_to_text;
 
@@ -51,11 +53,64 @@ impl TextImage {
     ///
     /// `target_width` is a "goal" width - the actual image will end up with a width that 
     /// preserves the original image's aspect ratio
+    ///
+    /// # Example
+    /// ```
+    /// # extern crate image;
+    /// # extern crate txtpic_lib;
+    /// # fn main() {
+    /// # use self::image::open;
+    /// # use std::path::Path;
+    /// # use txtpic_lib::character_set::CharacterSet;
+    /// # use txtpic_lib::text_image::TextImage;
+    /// // import necessary crates and modules...
+    ///
+    /// let img = open(Path::new("./example/cat.jpg")).unwrap();
+    /// let char_set = CharacterSet::preset_small();
+    /// let txt_img = TextImage::from(img, char_set, 80);
+    /// let cute_kitty = "\
+    /// ==xxxxxxx=======+++--++-+-.:--:--+:.-+---++++=xxxxxx=====xxxxxx==========++
+    /// =xxxxxxx====xx=x=++=++++++.++-:--=+--++++++++=====xxx=++=xxxxxx============
+    /// =xxxx=====xxxxxx+++++++=+--+x+:-=x+-+==+++=+===++=++++--+-+=xx====x========
+    /// =====x+++xxxx=+++++=++++=+:-x+:-xx-:=x=+-+========+++++---+================
+    /// +++++++--++++++=====++--=x=-=+-+=x--xxx+-+xxx======+++++---+===========++++
+    /// --+-++---+++++======x=-:=xx-+=--+x-+xxx-:+x=+=======++++=+++=xx============
+    /// +--+++---+++++x==+++=x+.-xx=-=--+=-+=xx=-=x=====xx=+++++==+===xxxxx========
+    /// --++-++++++===xxx===+=x--xxx+++--+=++xxx+=+-+xxx-++--++++++++=xxxxxxxxxxxxx
+    /// +++-+++=x=====x=++==++==+x=++++++++=+=xxxxx+-xxx++x=+=+++-+===xxxxxxxxxxxxx
+    /// ++--+=++x====++=+xxx++xxx=xx+==++++==x%++xxxxxx+-x=++++=x=-++xxxxxxxxxxxxxx
+    /// +++-++++++=xxxxx=+xxxxxx=:=x==+++++==xx+::-----+++--+===xxxx=xx%%%%%%%%%%%%
+    /// +++--=+++xx===+=xx+++++---+=+=========+-++=++-----++==xxxxx+=xxxxxxxxxxxxxx
+    /// ++---+===xxx==+++-++++===+-:-=xxx=xx==+:.--::---++==xx+--+++xxxxxxxxxxxxxxx
+    /// +++++++=xxxxxx=+=++-------::+==xx====+++---++++++xxxxxx=x=xxxxxxx%%%%%%%%%%
+    /// +----++++++++=xxxx=++++++-----+=====+--++-++=+==xxxxx=+=xxx%%%%%%%%%%%%xxxx
+    /// +==+--=++++=====xxxxx===+++=--+===+=++--===+===xxxxx==xxxxxxxxx%%%%%%%%%%%%
+    /// ++=+++++==++====xxxxxxx=+xxx=-==+++++=+=xx=x=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    /// -+++++++++========xxxxxx=x=xxxx=+x==-=xxxx===xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    /// +-+++--+++====xx=xxxxxxxxxxxxxxxx=-+xxxxxxxxxxxxxxxxxx========xxxxxxxxxxxxx
+    /// ++--++++++++=x=x====xxxxxxxxxxxxxx+=xxxxxxxxxxxxxxxxx=++++++-+=xxxxxxxxxxxx
+    /// ===+---+=+++++++=xxxxxxxxxxxxxxxxx==xxxxxxxxxxxxxxxxx==+++--+=====+xxxxxxxx
+    /// +++++-----+++++==++==xx=++++=xxxxxxxxx%xxxxxxxxxxxxx=+++++--+=x=x===xxxxxxx
+    /// ++++++------++=+-+=+=++++-+-+-++=xxxxxxxxxxxxxxxxxx+++----+++==xxx==xxxxxxx
+    /// ++==++++--+--+--++++++++++++++++========+==+=xxxxxx=++-::--+==xxxxxxxxxxxxx
+    /// ";
+    /// assert_eq!(txt_img.to_string(), cute_kitty);
+    /// # }
+    /// ```
     pub fn from(img: DynamicImage, char_set: CharacterSet, target_width: u32) -> TextImage {
         image_to_text(img, char_set, target_width)
     }
 
     /// Sets the character at (x, y) to c
+    /// 
+    /// # Example
+    /// ```
+    /// use txtpic_lib::text_image::TextImage;
+    ///
+    /// let mut txt_img = TextImage::new(5, 5);
+    /// txt_img.set_char(1, 1, 'M');
+    /// assert_eq!(txt_img[1][1], 'M');
+    /// ```
     pub fn set_char(&mut self, x: usize, y: usize, c: char) {
         self.chars[y][x] = c
     }
